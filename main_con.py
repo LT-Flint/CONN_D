f228c5b6cfbd2679f311a5c77a8f381b51b3124d
pyneng <mur_z@bk.ru>
2025-03-25 17:55:25 +0300

    Сделана циска запись команд. Рабочий вариант

--- main_con.py
+++ main_con.py
@@ -14,22 +14,81 @@
 
 """
 import yaml
-from netmiko import Netmiko
+import csv
+
+#from netmiko import Netmiko
+from netmiko import (Netmiko, NetmikoBaseException, NetmikoAuthenticationException)
+from paramiko.ssh_exception import SSHException
+from rich import inspect
 
 def send_show_command(device, command):
-    with Netmiko(**device) as ssh:
-        print(ssh.enable())
-        out = ssh.send_command(command)
-        
-    return out
+    try:
+        with Netmiko(**device) as ssh:
+            #inspect(ssh)
+            print(ssh.enable())
+            print(command)
+            out = ssh.send_command(command)
+            ssh.exit_enable_mode()
+        return out
+    except (NetmikoBaseException, NetmikoAuthenticationException, SSHException) as error:
+            #print(error)
+            print('Device ip = ' + device['host'] + ' not avaliable')
+
+def send_conf_commands(device, commands):
+    try:
+        with Netmiko(**device) as ssh:
+            ssh.enable()
+            out = ssh.send_config_set(commands)
+            return out
+    except (NetmikoBaseException, NetmikoAuthenticationException, SSHException) as error:
+            #print(error)
+            print('Device ip = ' + device['host'] + ' not avaliable')
+
 
 if __name__ == "__main__":
-    command_el = "sh ip int"
+    admins_dict = {
+        'admin1': 'admin1_PASS',
+        'admin2': 'admin2_PASS'
+        }
+    
+    commands_http_cisco = ["no ip http server", "no ip http secure-server"]
+    
+    admin_pass = ['username admin5 privilege 15 secret 5 $1$Gjzn$4rU4sT5jxTlWIjEg7KpaE.',
+'username admin1 privilege 15 secret 5 $1$7tVe$zu4W6hdA2oI.BDnOw6YMC0',
+'username admin3 privilege 15 secret 5 $1$8Fsa$fEmKDnhNa8kOhSd/3fpo2.',
+'username admintech privilege 15 secret 5 $1$M8sM$ybMAjGn1wjOl1SYBiOD2f.',
+'username admin10 privilege 15 secret 5 $1$uT7n$FEJJCjF6B3hJ0gocEacl4.'
+        
+        ]
+    
+    
+    command_el = "sh clo"
     command_hu = 'dis ip int br'
-    with open("devices.yaml") as f:
-        devices = yaml.safe_load(f)
+    
+    headers = ['device_type', 'host', 'username', 'password', 'secret','timeout']
+
+    with open('test_dev.csv', 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file, fieldnames=headers)  # задаём ключи вручную
+        devices = [row for row in reader]
+
+
+       
+    
+    
+    
+    
+    
+    
+    #with open("devices.yaml") as f:
+    #    devices = yaml.safe_load(f)
     for dev in devices:
+        print(dev)
         if dev['device_type'] == 'eltex':
-            print(send_show_command(dev, command_el))
+            print(send_show_command(dev, "show run | i admin"))
+
         elif dev['device_type'] == 'huawei':
             print(send_show_command(dev, command_hu))
+        elif dev['device_type'] == 'cisco_ios':
+            #print(send_show_command(dev, "show run | i admin"))
+            send_conf_commands(dev, commands_http_cisco)
+            send_conf_commands(dev, admin_pass)
